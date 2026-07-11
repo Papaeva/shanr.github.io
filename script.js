bbe156607f6b84acd1a0643ff6edf81b68e67c34
Papaeva <116063862+Papaeva@users.noreply.github.com>
2026-07-11 16:25:49 +0800

    Commit smitlter

--- script.js
+++ script.js
@@ -12,24 +12,7 @@ if (hamburger && navMenu) {
     navMenu.classList.toggle('close');
   });
 }
-// const cards = document.querySelectorAll('.card');
-// cards.forEach(function(card) {
-//   const img = card.querySelector('img');
-//   const text = card.querySelector('p');
-//   if (img && text) {
-//     img.addEventListener = function() {
-//       alert(text.innerText);
-//     };
-//   }
-// });
 
-// const mobileMenuBtn = document.querySelector('#mobileMenuBtn');
-// const navLinks = document.querySelector('#navLinks');
-// mobileMenuBtn.addEventListener('click', () => {
-//   navLinks.classList.toggle('active');
-//   mobileMenuBtn.innerHTML = navLinks.classList.contains('active')
-//   ? 
-// })
 
 (function() {
   const btn = document.getElementById('topBtn');
